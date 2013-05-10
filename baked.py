059a4ebcddc9d91dbd39bd3da5328a19cf01d861
Andy McKay <amckay@mozilla.com>
2013-05-10 16:28:47 -0700

    use standard stdlib list

--- baked.py
+++ baked.py
@@ -8,6 +8,32 @@ from collections import defaultdict, namedtuple
 from glob import glob
 from subprocess import PIPE, Popen
 
+stdlib = [
+    'abc', 'anydbm', 'argparse', 'array',
+    'asynchat', 'asyncore', 'atexit', 'base64', 'BaseHTTPServer',
+    'bisect', 'bz2', 'calendar', 'cgitb', 'cmd', 'codecs', 'collections',
+    'commands', 'compileall', 'ConfigParser', 'contextlib', 'Cookie',
+    'copy', 'cPickle', 'cProfile', 'cStringIO', 'csv', 'datetime',
+    'dbhash', 'dbm', 'decimal', 'difflib', 'dircache', 'dis', 'doctest',
+    'dumbdbm', 'EasyDialogs', 'exceptions', 'filecmp', 'fileinput',
+    'fnmatch', 'fractions', 'functools', 'gc', 'gdbm', 'getopt',
+    'getpass', 'gettext', 'glob', 'grp', 'gzip', 'hashlib', 'heapq',
+    'hmac', 'imaplib', 'imp', 'inspect', 'itertools', 'json', 'linecache',
+    'locale', 'logging', 'mailbox', 'math', 'mhlib', 'mmap',
+    'multiprocessing', 'operator', 'optparse', 'os', 'os.path', 'pdb',
+    'pickle', 'pipes', 'pkgutil', 'platform', 'plistlib', 'pprint',
+    'profile', 'pstats', 'pwd', 'pyclbr', 'pydoc', 'Queue', 'random',
+    're', 'readline', 'resource', 'rlcompleter', 'robotparser', 'sched',
+    'select', 'shelve', 'shlex', 'shutil', 'signal', 'SimpleXMLRPCServer',
+    'site', 'sitecustomize', 'smtpd', 'smtplib', 'socket', 'SocketServer',
+    'sqlite3', 'string', 'StringIO', 'struct', 'subprocess', 'sys',
+    'sysconfig', 'tabnanny', 'tarfile', 'tempfile', 'textwrap',
+    'threading', 'time', 'timeit', 'trace', 'traceback', 'unittest',
+    'urllib', 'urllib2', 'urlparse', 'usercustomize', 'uuid', 'warnings',
+    'weakref', 'webbrowser', 'whichdb', 'xml', 'xml.etree.ElementTree',
+    'xmlrpclib', 'zipfile', 'zipimport', 'zlib'
+]
+
 
 class Parser(object):
 
@@ -19,6 +45,7 @@ class Parser(object):
             self.file = filename[len(cwd):]
         self.order = ('stdlib', 'unknown')
         self.modules = {}
+        [self.modules.__setitem__('stdlib', k) for k in stdlib]
         self.parsed = []
         self.fallback = 'unknown'
         self.record = namedtuple('Tree', ('type', 'module', 'name', 'number',
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='baked',
-    version='0.1.3.2',
+    version='0.1.3.3',
     description='Import order detection',
     long_description=open('README.rst').read(),
     author='Andy McKay',
